113eca2038603371b6f64b237b4e7143a198c262
slim <slimabida21@gmail.com>
2024-12-05 20:11:36 +0100

    configuration du fichier register.php

--- register.php
+++ register.php
@@ -11,49 +11,63 @@
                     <div class="col-lg-7">
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
                             <div class="card-header">
-                                <h3 class="text-center font-weight-light my-4">Create Account</h3>
+                                <h3 class="text-center font-weight-light my-4">Créer votre compte</h3>
                             </div>
                             <div class="card-body">
                                 <form>
                                     <div class="row mb-3">
                                         <div class="col-md-6">
                                             <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" />
-                                                <label for="inputFirstName">First name</label>
+                                                <input class="form-control" id="inputFirstName" type="text" placeholder="Veuillez entrer votre prénom" name="prenom" />
+                                                <label for="inputFirstName">Prénom</label>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-floating">
-                                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" />
-                                                <label for="inputLastName">Last name</label>
+                                                <input class="form-control" id="inputLastName" type="text" placeholder="Veuillez entrer votre nom " name="nom" />
+                                                <label for="inputLastName">Nom</label>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                        <label for="inputEmail">Email address</label>
+                                        <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" name="email" />
+                                        <label for="inputEmail">Adresse email</label>
                                     </div>
                                     <div class="row mb-3">
                                         <div class="col-md-6">
                                             <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
-                                                <label for="inputPassword">Password</label>
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Mot de passe " name="password" />
+                                                <label for="inputPassword">Mot de passe</label>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
-                                                <label for="inputPasswordConfirm">Confirm Password</label>
+                                                <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirmation du mot de passe" name="password_confirm" />
+                                                <label for="inputPasswordConfirm">Confirmer mot de passe</label>
                                             </div>
                                         </div>
                                     </div>
+
+                                    <div class="form-floating mb-3">
+                                        <textarea class="form-control" id="biographie" type="text" placeholder="courte biograhie" name="biographie"></textarea>
+                                        <label for="biographie">Biographie</label>
+                                    </div>
+                                    <div>
+                                        <label for="photo_profil">Photo de Profile</label>
+                                        <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+                                        <input type="file" name="photo_profil" id="photo_profil">
+                                    </div>
+
+
+
+
                                     <div class="mt-4 mb-0">
-                                        <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
+                                        <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">S'inscrire</a></div>
                                     </div>
                                 </form>
                             </div>
                             <div class="card-footer text-center py-3">
-                                <div class="small"><a href="login.html">Have an account? Go to login</a></div>
+                                <div class="small"><a href="login.php">Avez vous déjà un compte ? Connectez-vous</a></div>
                             </div>
                         </div>
                     </div>
